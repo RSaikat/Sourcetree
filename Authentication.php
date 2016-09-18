882867118ccdc9573566e8c2e57823f2f4162243
DivineIT-PC\DivineIT <rubayat@divine-it.net>
2016-09-18 14:04:07 +0600

    login page

---
+++ Authentication.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login information</title>
+	<h1>Please Login</h1>
+</head>
+<body>
+<body style="background-color:lightblue;">
+	<form action='check.php' method="post">
+		User name<br>
+		<input type="text" name ="username" id="username"><br></br>
+		Password<br>
+		<input type="text" name ="password" id="pasword"><br></br>
+		<input type="submit" value ="submit" name="submit">
+	</form>
+</body>
+</html>
